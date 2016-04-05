8a1abbc877882210c112783e6a6f81893c1031d5
sarkiroka <sarki_roka@freemail.hu>
2016-04-05 07:49:18 +0200

    gettype teszt

---
+++ test/lib/common/get-type.js
@@ -0,0 +1,30 @@
+require('expectations');
+var testfile = require('../../../lib/common/get-type');
+describe('get type', function () {
+	var values = {
+		null: {value: null, type: 'null'},
+		zero: {value: 0, type: 'number'},
+		number: {value: 42, type: 'number'},
+		negative: {value: -42, type: 'number'},
+		float: {value: 1e3, type: 'number'},
+		emptyString: {value: '', type: 'string'},
+		string: {value: 'sarkiroka', type: 'string'},
+		emptyObject: {value: {}, type: 'object'},
+		filledObject: {value: {a: 42}, type: 'object'},
+		emptyArray: {value: [], type: 'array'},
+		filledArray: {value: [1, 2, 3], type: 'array'},
+		true: {value: true, type: 'boolean'},
+		false: {value: false, type: 'boolean'},
+		undefined: {type: 'undefined'}
+	};
+	for (var name in values) {
+		(function (name) {
+			it('handles "' + name + '" parameter', function () {
+				var item = values[name];
+				var result = testfile(item.value);
+				expect(typeof result).toEqual('string', 'should produce a string - type name');
+				expect(result).toEqual(item.type, 'should give "' + item.type + '" for value ' + item.value);
+			});
+		})(name);
+	}
+});
