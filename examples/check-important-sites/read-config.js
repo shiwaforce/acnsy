7083941145b791b94d1c35048347b3713a483ccb
sarkiroka <sarki_roka@freemail.hu>
2016-05-04 00:12:52 +0200

    egy konfig fájl felolvasása

---
+++ examples/check-important-sites/read-config.js
@@ -0,0 +1,10 @@
+/**
+ * @author sarkiroka on 2016.05.03.
+ */
+module.exports = function readConfig(params, callback) {
+	delete require.cache[require.resolve(params.configPath)];
+	var config = require(params.configPath);//if we are use fs for read the file, we must call the callback in the callback of fs function. the require is synchron
+	params.sites = config.sites;
+	params.tresholds = config.tresholds;
+	callback(null, params);
+};
