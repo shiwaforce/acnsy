2b128157c2318a95fab927fd226d43fcf2b6a93b
sarkiroka <sarki_roka@freemail.hu>
2016-05-24 21:24:57 +0200

    idődilettáció beépítése a tesztbe

--- test/end-to-end/timeouts.js
+++ test/end-to-end/timeouts.js
@@ -3,21 +3,21 @@
  */
 var acnsy = require('../../index_v2');
 var addToResultFactory = require('./helper/add-to-result-factory');
-var ifTaskFactory=require('./helper/if-task-factory');
+var ifTaskFactory = require('./helper/if-task-factory');
 describe('acnsy timeout', function () {
 	it('pass error to the end task when timeout reached - serial case', function (done) {
 		var params = {a: 1, result: 0};
 		var timeA = 100;
-		var currentTimeout=50;
+		var currentTimeout = 50;
 		var addA = addToResultFactory(timeA, 'a');
-		addA.timeout=currentTimeout;
+		addA.timeout = currentTimeout;
 		var tasks = [addA];
 		var startTime = Date.now();
 
 		acnsy(params, tasks, function (err, givenParams) {
 			var endTime = Date.now();
 			expect(err).toBeTruthy('should pass an error');
-			var timeDiff = endTime - startTime;
+			var timeDiff = endTime - startTime + 1;
 			expect(timeDiff).not.toBeLessThan(currentTimeout, 'should wait the timeout seted time');
 			expect(timeDiff).toBeLessThan(timeA, 'should stop execution when timeout reached');
 			done();
@@ -27,16 +27,16 @@ describe('acnsy timeout', function () {
 	it('pass error to the end task when timeout reached - parallel case', function (done) {
 		var params = {a: 1, result: 0};
 		var timeA = 200;
-		var currentTimeout=50;
+		var currentTimeout = 50;
 		var addA = addToResultFactory(timeA, 'a');
-		addA.timeout=currentTimeout;
+		addA.timeout = currentTimeout;
 		var tasks = [[addA]];
 		var startTime = Date.now();
 
 		acnsy(params, tasks, function (err, givenParams) {
 			var endTime = Date.now();
 			expect(err).toBeTruthy('should pass an error');
-			var timeDiff = endTime - startTime;
+			var timeDiff = endTime - startTime + 1;
 			expect(timeDiff).not.toBeLessThan(currentTimeout, 'should wait the timeout seted time');
 			expect(timeDiff).toBeLessThan(timeA, 'should stop execution when timeout reached');
 			done();
@@ -45,9 +45,9 @@ describe('acnsy timeout', function () {
 
 	it('pass error to the end task when timeout reached - if-condition case', function (done) {
 		var time = 100;
-		var currentTimeout=50;
+		var currentTimeout = 50;
 		var ifTask = ifTaskFactory(true, time);
-		ifTask.timeout=currentTimeout;
+		ifTask.timeout = currentTimeout;
 		var tasks = [acnsy.if(ifTask)];
 		var startTime = Date.now();
 
@@ -55,7 +55,7 @@ describe('acnsy timeout', function () {
 		acnsy(tasks, function (err, givenParams) {
 			var endTime = Date.now();
 			expect(err).toBeTruthy('should pass an error');
-			var timeDiff = endTime - startTime;
+			var timeDiff = endTime - startTime + 1;
 			expect(timeDiff).not.toBeLessThan(currentTimeout, 'should wait the timeout seted time');
 			expect(timeDiff).toBeLessThan(time, 'should stop execution when timeout reached');
 			done();
