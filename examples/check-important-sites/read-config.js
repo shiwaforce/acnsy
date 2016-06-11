8a6d1c0a48b755984bf3e90ea5b62ffe7229681e
sarkiroka <sarki_roka@freemail.hu>
2016-06-11 21:23:09 +0200

    possible errors config reads are catched

--- examples/check-important-sites/read-config.js
+++ examples/check-important-sites/read-config.js
@@ -1,10 +1,15 @@
 /**
+ * reads the config
  * @author sarkiroka on 2016.05.03.
  */
 module.exports = function readConfig(params, callback) {
 	delete require.cache[require.resolve(params.configPath)];
-	var config = require(params.configPath);//if we are use fs for read the file, we must call the callback in the callback of fs function. the require is synchron
-	params.sites = config.sites;
-	params.tresholds = config.tresholds;
-	callback(null, params);
+	try {
+		var config = require(params.configPath);//if we are use fs for read the file, we must call the callback in the callback of fs function. the require is synchron
+		params.sites = config.sites;
+		params.tresholds = config.tresholds;
+		callback(null, params);
+	} catch (e) {
+		callback(e, params);
+	}
 };
