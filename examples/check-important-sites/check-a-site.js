30cb9cea4f6cb964c58120b687dc926ca19eac64
sarkiroka <sarki_roka@freemail.hu>
2016-06-14 23:25:13 +0200

    http example http part begins

--- examples/check-important-sites/check-a-site.js
+++ examples/check-important-sites/check-a-site.js
@@ -1,8 +1,25 @@
 /**
+ * check a site
  * @author sarkiroka on 2016.05.03.
  */
+var http=require('http');
 module.exports = function (params, callback) {
 	var siteDescriptor = this.item;
+	var httpOptions={
+		hostname: item.host,
+		port: 80,
+		path: '/',
+		method: 'GET'
+	};
+	var req=http.request(httpOptions);
 	console.log(siteDescriptor);
-	callback(null, params);
+	req.on('error',function(){
+		callback(null, params);
+
+	});
+	req.on('end',function(){
+		callback(null, params);
+
+	})
+	req.end();
 };
